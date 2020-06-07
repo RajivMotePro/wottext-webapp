fd8bb9f250b63a16204e7528d84cf1cec959499f
RajivMotePro <rajivmotepro@gmail.com>
2020-06-06 19:11:50 -0500

    Add book page, handlers for /book, /chapter-*

--- wottextapp.py
+++ wottextapp.py
@@ -13,4 +13,28 @@ def search():
 	query = request.args.get('query')
 	return render_template('search-results.html', query=query)
 
+@app.route('/book')
+def book():
+	bookNum = request.args.get('bkNum')
+	title = "TODO Fetch title"
+	return render_template('book.html', bkNum=bookNum, title=title)
+
+@app.route('/chapter-entities')
+def chapter_entities():
+	bookNum = request.args.get('bkNum')
+	chapterNum = request.args.get('chNum')
+	return render_template('chapter-entities.html', bkNum=bookNum, chNum=chapterNum)
+
+@app.route('/chapter-topics')
+def chapter_topics():
+	bookNum = request.args.get('bkNum')
+	chapterNum = request.args.get('chNum')
+	return render_template('chapter-topics.html', bkNum=bookNum, chNum=chapterNum)
+
+@app.route('/chapter-sentiment')
+def chapter_sentiment():
+	bookNum = request.args.get('bkNum')
+	chapterNum = request.args.get('chNum')
+	return render_template('chapter-sentiment.html', bkNum=bookNum, chNum=chapterNum)
+
 app.run(debug=True)
